bd824a2f5c4ca7469f0ce62c8e6553f92dce3db2
Benjamin Liden <benjamin.c.liden@gmail.com>
2024-05-30 10:43:40 -0400

    fix: raise error if lut isn't found

--- src/rumraisin/image/filter.py
+++ src/rumraisin/image/filter.py
@@ -8,6 +8,8 @@ def antique_filter(image: Image, intensity: float) -> Image:
     The image filtering for the "Antique" look
     """
     lut = get_lut("Faded")
+    if lut is None:
+        raise SystemError("LUT 'Faded' could not be found")
     amplified = amplify_lut(lut, intensity)
     image = image.filter(amplified)
     return image
